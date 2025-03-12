6f751774a5cf29c6736b93bcca133d6302c1a30c
RM <rizayevamm@gmail.com>
2025-03-13 03:18:58 +0500

    app creation

--- therestaurant/settings.py
+++ therestaurant/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #local:
+
+    'restaurantsystem'
+
 ]
 
 MIDDLEWARE = [
@@ -76,7 +81,7 @@ WSGI_APPLICATION = 'therestaurant.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
