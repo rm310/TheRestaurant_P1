7b13e5243df07362e90d77d9a6585460bef2d6c0
rm310 <muslimarizayeva@webster.edu>
2025-09-06 14:14:14 +0500

    Docker compose

--- therestaurant/settings.py
+++ therestaurant/settings.py
@@ -28,6 +28,7 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL='accounts.CustomUser'
 
+SECRET_KEY = 'test'
 
 
 
@@ -90,14 +91,21 @@ WSGI_APPLICATION = 'therestaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'restaurantsystem',
+#         'USER': 'postgres',
+#         'PASSWORD': 'futureNewYorker.310',
+#         'HOST': '127.0.0.1',
+#         "PORT": '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'restaurantsystem',
-        'USER': 'postgres',
-        'PASSWORD': 'futureNewYorker.310',
-        'HOST': '127.0.0.1',
-        "PORT": '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite3",
     }
 }
 
@@ -146,6 +154,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
